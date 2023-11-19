95fc6c50fb1ed9c04c6254956400ede83a8951a9
Attila Diós <xatixatix@gmail.com>
2023-11-19 15:51:33 +0100

    Added some fragments but app keeps crashing on startup without error message

---
+++ app/src/main/java/com/xatixatix/clickerpilot/BaseGameFragment.kt
@@ -0,0 +1,7 @@
+package com.xatixatix.clickerpilot
+
+import androidx.fragment.app.Fragment
+
+class BaseGameFragment : Fragment(R.layout.fragment_base_game) {
+
+}
---
+++ app/src/main/java/com/xatixatix/clickerpilot/StartMenuFragment.kt
@@ -0,0 +1,7 @@
+package com.xatixatix.clickerpilot
+
+import androidx.fragment.app.Fragment
+
+class StartMenuFragment : Fragment(R.layout.fragment_start_menu) {
+
+}
